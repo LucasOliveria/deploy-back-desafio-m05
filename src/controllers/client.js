92f376d28ab8c0710b55f5339f760f7fc7137f21
Lucas Oliveira <lucasoliveiralog@hotmail.com>
2023-07-26 09:20:01 -0300

    correção chamada para a tabela clients

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -4,8 +4,8 @@ const createClient = async (req, res) => {
 
   const client = req.body
   try {
-    const findEmail = await knex("client").where({ email: client.email }).first();
-    const findcpf = await knex("client").where({ cpf: client.cpf }).first();
+    const findEmail = await knex("clients").where({ email: client.email }).first();
+    const findcpf = await knex("clients").where({ cpf: client.cpf }).first();
 
     if (findEmail) {
       return res.status(400).json("Já existe um cliente cadastrado com este e-mail.")
@@ -14,7 +14,7 @@ const createClient = async (req, res) => {
       return res.status(400).json("Já existe um cliente cadastrado com este CPF.")
     }
 
-    await knex("client")
+    await knex("clients")
       .insert(client)
       .returning("*");
     return res.status(201).json("Cliente cadastrado com sucesso.")
@@ -27,7 +27,7 @@ const createClient = async (req, res) => {
 const clientList = async (req, res) => {
 
   try {
-    const allClients = await knex('client')
+    const allClients = await knex('clients')
     return res.status(200).json(allClients)
   } catch (err) {
     return res.status(500).json("Erro interno do servidor.")
